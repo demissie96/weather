2d67c72e42066a421fd78fec1f3afbcd4af31285
Johannes Demissie <demissie.johannes@gmail.com>
2022-09-30 00:23:33 +0200

    Change icon according to current weather.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import axios from "axios";
 const openWeatherUrl = "https://api.openweathermap.org/data/2.5/weather";
 const openWeatherAPIKey = process.env.REACT_APP_OPEN_WEATHER_KEY;
 let city;
+let iconNum;
 let weatherID;
 let weatherMain;
 let weatherCity;
@@ -27,14 +28,36 @@ function App() {
       )
       .then(function (response) {
         // handle success
-        console.log(response.data);
         weatherID = response.data.weather[0].id;
         weatherMain = response.data.weather[0].main;
         weatherCity = response.data.name;
         weatherTemp = Math.round(response.data.main.temp);
         weatherHumidity = response.data.main.humidity;
         weatherCountry = response.data.sys.country;
-        weatherIcon = `https://openweathermap.org/img/wn/11d@2x.png`;
+        if (weatherID >= 500 && weatherID <= 504) {
+          iconNum = "10";
+        } else if (weatherID >= 520 && weatherID <= 531) {
+          iconNum = "09";
+        } else if (weatherID === 511) {
+          iconNum = "13";
+        } else if (weatherID >= 200 && weatherID <= 232) {
+          iconNum = "11";
+        } else if (weatherID >= 300 && weatherID <= 321) {
+          iconNum = "09";
+        } else if (weatherID >= 600 && weatherID <= 622) {
+          iconNum = "13";
+        } else if (weatherID >= 700 && weatherID <= 781) {
+          iconNum = "50";
+        } else if (weatherID >= 803 && weatherID <= 804) {
+          iconNum = "04";
+        } else if (weatherID === 800) {
+          iconNum = "01";
+        } else if (weatherID === 801) {
+          iconNum = "02";
+        } else if (weatherID === 802) {
+          iconNum = "03";
+        }
+        weatherIcon = `https://openweathermap.org/img/wn/${iconNum}d@2x.png`;
         console.log(
           `${weatherCity}, ${weatherCountry}, ${weatherID}, ${weatherMain}, ${weatherTemp}, ${weatherHumidity}`
         );
